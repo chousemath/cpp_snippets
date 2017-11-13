de226e94047d0c4726ac200cfde84560642da2a9
Joseph Sungpil Choi <chousemath@gmail.com>
2017-11-13 23:25:57 +0900

    Add a Google C++ challenge
    This commit contains a simple coding challenge from the Google C++ guide

---
+++ cpp-practice-2.cpp
@@ -0,0 +1,16 @@
+// using for-loop to print hello
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 4; j++) {
+      cout << "hello ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
